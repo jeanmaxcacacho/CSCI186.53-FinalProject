ef3864e8eedab236ca59b09a3f44202f8c46f696
jeanmaxcacacho <maxcacacho@gmail.com>
2025-07-13 19:01:04 +0800

    project init

--- Main.sol
+++ Main.sol
@@ -1,13 +1,6 @@
-// SPDX-License Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.2;
 
 contract Main {
 
-    function helloWorld() public pure returns (string memory) {
-        return "hello world";
-    }
-
-    constructor() {
-        helloWorld();
-    }
 }
